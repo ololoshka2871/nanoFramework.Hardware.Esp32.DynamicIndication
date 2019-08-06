304d05f629ad9128120e86d820a99d35a2fb686f
rubanok ololoshka <Lolka01@DESKTOP-1KOL192>
2019-08-06 16:54:37 +0300

    Добавьте файлы проекта.

---
+++ nanoFramework.Hardware.Esp32.DyncmicIndication/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace nanoFramework.Hardware.Esp32.DyncmicIndication
+{
+	public class nanoFramework.Hardware.Esp32.DyncmicIndication
+    {
+        // Browse our samples repository: https://github.com/nanoframework/samples
+        // Check our documentation online: https://docs.nanoframework.net/
+        // Join our lively Discord community: https://discord.gg/gCyBu8T
+    }
+}
